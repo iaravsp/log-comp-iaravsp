352a03c11ed0d8b2fd7fdf3d8574584e5f3caacf
iara.vsp <iaravsp@al.insper.edu.br>
2026-03-04 11:28:12 -0300

    Roteiro 2 - v0.0.0

--- main.py
+++ main.py
@@ -1,55 +1,76 @@
 import sys
-def main ():
-    if len(sys.argv) < 2:
-        return
-    entrada = sys.argv[1]
 
-    numeros = '0123456789'
-    operadores = ['+', '-']
-    itens = []
+class Token:
+     def __init__(self, tipo, valor):
+        self.type: str = tipo
+        self.value: int = valor
 
-    i = 0
-    while i < len(entrada):
-        if entrada[i] == ' ':
-            i += 1
-            continue
-        elif entrada[i] in numeros:
-            num = ''    
-            while i < len(entrada) and entrada[i] in numeros:
-                num += entrada[i]
-                i+=1
-            itens.append(num)
-            continue
+# análise léxica 
+class Lexer:
+    def __init__(self, source: str):
+        self.source = source#código de entrada
+        self.position = 0
+        self.next = None
 
-        elif entrada[i] in operadores:
-            itens.append(entrada[i])
-            i+=1    
+    def select_next(self):
+        while self.position < len(self.source) and self.source[self.position] == ' ':
+            self.position +=1
+        if self.position >= len(self.source):
+            self.next = Token('EOF','')
+            self.position +=1
+            return
+        if self.source[self.position] == '+':
+            self.next = Token('PLUS', '+')
+            self.position +=1
+        elif self.source[self.position] == '-': 
+            self.next = Token('MINUS', '-')
+            self.position +=1
+        elif self.source[self.position].isdigit():
+            digito = str(self.source[self.position])
+            self.position +=1
+            while  self.position < len(self.source) and self.source[self.position].isdigit():
+                digito = digito + self.source[self.position]
+                self.position +=1
+            self.next = Token('INT', int(digito))
         else:
-            raise Exception()
-        
-    # print(itens)
+            raise ValueError(f"[Lexer] Invalid Symbol {self.source[self.position]}")
 
-    resultado = int(itens[0])
-    j=1
-    while j < len(itens):
+class Parser():
+    lexer = None
 
-        op = itens[j]
-        if op not in operadores:
-            raise Exception()
-        
-        if j+1 >= len(itens):
-            raise Exception()
-        
-        proximo = int(itens[j+1])
+    @staticmethod
+    def parse_expression():
+        if Parser.lexer.next.type != 'INT':
+            raise ValueError(f"[Parser] Unexpected token {Parser.lexer.next.value}")
+        else: # é INT
+            resultado = Parser.lexer.next.value
+            Parser.lexer.select_next()
 
-        if op == '+':
-            resultado += proximo
-        elif op == '-':
-            resultado -= proximo
+        while Parser.lexer.next.type == "PLUS" or Parser.lexer.next.type == "MINUS":
+            operador = '+' if Parser.lexer.next.type == "PLUS" else '-'
+            Parser.lexer.select_next()
+            if Parser.lexer.next.type != 'INT':
+                raise ValueError(f"[Parser] Unexpected token {Parser.lexer.next.value}")
+            if operador == "+":
+                resultado += Parser.lexer.next.value
+            elif operador == "-":
+                resultado -= Parser.lexer.next.value
+            Parser.lexer.select_next()
+        return resultado
 
-        j+=2
-    
-    print(resultado)
+    @staticmethod
+    def run(code: str):
+        Parser.lexer = Lexer(code)
+        Parser.lexer.select_next()
+        pars = Parser.parse_expression()
+        if Parser.lexer.next.type != 'EOF':
+            raise ValueError(f"[Parser] Unexpected token {Parser.lexer.next.value}")
+        return pars
+
+def main():
+    entrada = sys.argv[1]
+    print(Parser.run(entrada))
 
 if __name__ == "__main__":
     main()
+    
