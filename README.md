a8e3628ab49403996d4a10600ed10fdfd7fd8b23
iara.vsp <iaravsp@al.insper.edu.br>
2026-04-22 13:59:47 -0300

    ok

--- README.md
+++ README.md
@@ -1,17 +1,30 @@
 ```ebnf
 
 PROGRAM = { STATEMENT } ;
-STATEMENT = ((IF, "(", BOOLEXPRESSION, ")", STATEMENT, ("ELSE", STATEMENT) | ε) | (WHILE, "(", BOOLEXPRESSION, ")", STATEMENT) | (IDENTIFIER, "=", BOOLEXPRESSION) | (PRINT, "(", BOOLEXPRESSION, ")") | ε), EOL ;
-BOOLEXPRESSION = BOOLTERM, { "||", BOOLTERM } ;
-BOOLTERM = RELEXPRESSION, { "&&", RELEXPRESSION } ;
-RELEXPRESSION = EXPRESSION, ("==" | "<" | ">"), EXPRESSION ;
-EXPRESSION = TERM, { ("+" | "-"), TERM } ;
+
+STATEMENT = ( "iden", "=", BOOLEXPRESSION
+          | "if", "(", BOOLEXPRESSION, ")", "then", { STATEMENT }, ( "end" | "else", { STATEMENT }, "end" )
+          | "local", "iden", "type", [ "=", BOOLEXPRESSION ]
+          | "while", "(", BOOLEXPRESSION, ")", "do", { STATEMENT }, "end"
+          | "print", "(", BOOLEXPRESSION, ")"
+          | BLOCK ), "EOL" ;
+
+BLOCK = "do", { STATEMENT }, "end" ;
+
+BOOLEXPRESSION = BOOLTERM, { "or", BOOLTERM } ;
+
+BOOLTERM = RELEXPRESSION, { "and", RELEXPRESSION } ;
+
+RELEXPRESSION = EXPRESSION, [ ("==" | "<" | ">"), EXPRESSION ] ;
+
+EXPRESSION = TERM, { ("+" | "-" | ".."), TERM } ;
+
 TERM = FACTOR, { ("*" | "/"), FACTOR } ;
-FACTOR = ("+" | "-"), FACTOR | "(", BOOLEXPRESSION, ")" | NUMBER | READ, "(", ")" ;
-NUMBER = DIGIT, {DIGIT} ;
-DIGIT = 0 | 1 | ... | 9 ;
-IDENTIFIER = LETTER, {LETTER | DIGIT | "_"} ;
-LETTER = a | b | ... | z | A | B | ... | Z ;
+
+FACTOR = "int" | "string_val" | "bool_val" | "iden" 
+       | ("+" | "-" | "not"), FACTOR 
+       | "(", BOOLEXPRESSION, ")" 
+       | "read", "(", ")" ;
 
 ```
 ![Diagrama Sintático do Compilador](image.png) 
--- main.py
+++ main.py
@@ -15,12 +15,21 @@ class SymbolTable:
             raise ValueError("[Semantic] - A variável não foi definida")
         return self.table[name]
     def set_value(self, name, value):
-        self.table[name] = Variable(value)
+        if name not in self.table.keys():
+            raise ValueError("[Semantic] - A variável não foi declarada")
+        if self.table[name].type != value.type:
+            raise ValueError(f"[Semantic] Erro de tipo. Esperado {self.table[name].type}, recebido {value.type}")
+        self.table[name].value = value
+    def create_variable(self, name, value, type):
+        if name in self.table.keys():
+            raise ValueError("[Semantic] - A variável já foi definida")
+        self.table[name] = Variable(value, type)
         
 class Variable:
-    def __init__(self, value):
+    def __init__(self, value, type):
         self.value = value 
-
+        self.type = type
+        
 class Node:
     def __init__(self, value, children):
         self.value = value
@@ -32,38 +41,78 @@ class Identifier(Node):
     def __init__(self, value, children):
         super().__init__(value,[])
     def evaluate(self, st: SymbolTable):
-        return st.get_value(self.value).value
+        return st.get_value(self.value)
     
 class Assignment(Node):
     def __init__(self, value, children):
         super().__init__(value,children)
     def evaluate(self, st: SymbolTable):
-        filho1  = self.children[0].value
-        filho2  = self.children[1].evaluate(st)
-        st.set_value(filho1, filho2)
+        nome = self.children[0].value
+        resultado_var = self.children[1].evaluate(st)
+        st.set_value(nome, resultado_var) #
 
 class Print(Node):
     def __init__(self, value, children):
         super().__init__(value,children)
     def evaluate(self, st: SymbolTable):
-        print(self.children[0].evaluate(st))
+        print(self.children[0].evaluate(st).value)
 
 class IntVal(Node):
     def __init__(self, value, children):
         super().__init__(value, [])
     def evaluate(self, st: SymbolTable):
-        return self.value
+        return Variable(self.value,'number')
+    
+class BoolVal(Node):
+    def __init__(self, value, children):
+        super().__init__(value, [])
+    def evaluate(self, st: SymbolTable):
+        return Variable(self.value,'boolean')
+
+class StrVal(Node):
+    def __init__(self, value, children):
+        super().__init__(value, [])
+    def evaluate(self, st: SymbolTable):
+        return Variable(self.value,'string')
+    
+class VarDec(Node):
+    def __init__(self, value, children):
+        super().__init__(value, children)
+    def evaluate(self, st):     
+        nome_variavel = self.children[0].value
+        #pegamos o tipo da variável que está guardado no valor do próprio VarDec
+        tipo_variavel = self.value
+        if len(self.children) == 1:
+            valor_padrao = 0
+            if tipo_variavel == "string": valor_padrao = ""
+            if tipo_variavel == "boolean": valor_padrao = False
+            st.create_variable(nome_variavel, valor_padrao, tipo_variavel)
+        elif len(self.children) == 2:
+            filho2  = self.children[1].evaluate(st)
+            if filho2.type != tipo_variavel:
+                raise ValueError(f"[Semantic] Erro na declaração. Variável '{nome_variavel}' é do tipo '{tipo_variavel}', mas recebeu '{filho2.type}'.")
+            st.create_variable(nome_variavel, filho2.value, tipo_variavel)
+        
+    
     
 class UnOp(Node):
     def __init__(self, value, children):
         super().__init__(value, children)
     def evaluate(self, st: SymbolTable):
-        res = self.children[0].evaluate(st)
-        if self.value == '-': 
-            return -res
-        if self.value == 'not':
-            return int(not res)
-        return res
+        res_var = self.children[0].evaluate(st) # Variável resultante da avaliação do filho -> Var(value, type)
+        if self.value in ['-', '+'] and res_var.type != 'number':
+            raise ValueError("[Semantic] Operação inválida para o tipo do operando")
+        else:
+            if self.value == '-': 
+                return Variable(-res_var.value, res_var.type)
+            if self.value == '+': 
+                return Variable(res_var.value, res_var.type)
+        if self.value == 'not' and res_var.type != 'boolean':
+            raise ValueError("[Semantic] Operação inválida para o tipo do operando")
+        else:
+            if self.value == 'not':
+                return Variable(not res_var.value, 'boolean')
+        
 
 class BinOp(Node):
     def __init__(self, value, children):
@@ -71,30 +120,36 @@ class BinOp(Node):
     def evaluate(self, st: SymbolTable):
         filho1_result  = self.children[0].evaluate(st)
         filho2_result  = self.children[1].evaluate(st)
-        if self.value == '+': 
-            return filho1_result + filho2_result
-        if self.value == '-':
-            return filho1_result - filho2_result
-        if self.value == '*': 
-            return filho1_result * filho2_result
-        if self.value == '/': 
-            if filho2_result == 0:
-                raise ValueError("[Semantic] divisão por zero não permitida")
-            return filho1_result // filho2_result
-        if self.value == '^':
-            return filho1_result ^ filho2_result
-        if self.value == '**':
-            return filho1_result ** filho2_result
-        if self.value == '==': 
-            return int(filho1_result == filho2_result)
-        if self.value == '>': 
-            return int(filho1_result > filho2_result)
-        if self.value == '<': 
-            return int(filho1_result < filho2_result)
-        if self.value == 'or': 
-            return int(filho1_result or filho2_result)
-        if self.value == 'and': 
-            return int(filho1_result and filho2_result)
+        
+        if self.value in ['+', '-', '*', '/', '^', '**', '>', '<']:
+            if filho1_result.type != 'number' or filho2_result.type != 'number':
+                raise ValueError(f"[Semantic] Operação '{self.value}' exige que ambos sejam 'number'. Recebeu {filho1_result.type} e {filho2_result.type}.")
+            
+            if self.value == '+': return Variable(filho1_result.value + filho2_result.value, 'number')
+            if self.value == '-': return Variable(filho1_result.value - filho2_result.value, 'number')
+            if self.value == '*': return Variable(filho1_result.value * filho2_result.value, 'number')
+            if self.value == '/': 
+                if filho2_result.value == 0: raise ValueError("[Semantic] Divisão por zero não permitida")
+                return Variable(filho1_result.value // filho2_result.value, 'number')
+            if self.value == '^': return Variable(filho1_result.value ^ filho2_result.value, 'number')
+            if self.value == '**': return Variable(filho1_result.value ** filho2_result.value, 'number')
+            
+            if self.value == '>': return Variable(filho1_result.value > filho2_result.value, 'boolean')
+            if self.value == '<': return Variable(filho1_result.value < filho2_result.value, 'boolean')
+
+        if self.value == '==':
+
+            return Variable(filho1_result.value == filho2_result.value, 'boolean')
+
+        if self.value in ['or', 'and']:
+            if filho1_result.type != 'boolean' or filho2_result.type != 'boolean':
+                raise ValueError(f"[Semantic] Operação '{self.value}' exige 'boolean'. Recebeu {filho1_result.type} e {filho2_result.type}.")
+            if self.value == 'or': return Variable(filho1_result.value or filho2_result.value, 'boolean')
+            if self.value == 'and': return Variable(filho1_result.value and filho2_result.value, 'boolean')
+
+        if  self.value == '..':
+            return Variable(str(filho1_result.value) + str(filho2_result.value), 'string')
+            
         raise ValueError(f"[Semantic] Operador inválido {self.value}")
 
 class NoOp(Node):
@@ -105,26 +160,29 @@ class NoOp(Node):
 
 class If(Node):
     def evaluate(self, st: SymbolTable):
-        if self.children[0].evaluate(st):
+        if self.children[0].evaluate(st).value:
             self.children[1].evaluate(st)
         elif len(self.children) == 3:
             self.children[2].evaluate(st)
 
 class While(Node):
     def evaluate(self, st: SymbolTable):
-        while self.children[0].evaluate(st):
+        while self.children[0].evaluate(st).value:
             self.children[1].evaluate(st)
 
 class Read(Node):
     def evaluate(self, st: SymbolTable):
-        return int(input())
+        return Variable(int(input()), 'number')
+    
+
+
 
 class Token:
      def __init__(self, tipo, valor):
         self.type: str = tipo
         self.value: int = valor
 
-PALAVRAS_RESERVADAS = ["print", "if", "then", "else", "while", "do", "end", "read", "and", "or", "not"]
+PALAVRAS_RESERVADAS = ["print", "if", "then", "else", "while", "do", "end", "read", "and", "or", "not", "local","true","false", "string","number","boolean"]
 # análise léxica 
 class Lexer:
     def __init__(self, source: str):
@@ -176,6 +234,13 @@ class Lexer:
         elif self.source[self.position] == '<':
             self.next = Token('LT', '<')
             self.position += 1
+        elif self.source[self.position] == '.':
+            self.position += 1
+            if self.source[self.position] == '.':
+                self.next = Token('CONCAT', '..')
+                self.position += 1
+            else:
+                raise ValueError(f"[Lexer] Caractere inválido: '{self.source[self.position-1]}'")
         elif self.source[self.position].isdigit(): 
             num_str = ''
             while self.position < len(self.source) and self.source[self.position].isdigit():
@@ -197,12 +262,31 @@ class Lexer:
                     tipo_token = "OPEN_BRA"
                 elif palavra_lida == "end":
                     tipo_token = "CLOSE_BRA"
+                elif palavra_lida in ["string", "number", "boolean"]:
+                    tipo_token = "TYPE"
+                elif palavra_lida in ["true", "false"]:
+                    tipo_token = "BOOL"
+                elif palavra_lida == "local":
+                    tipo_token = "VAR"
                 else:
                     tipo_token = palavra_lida.upper()
                     
                 self.next = Token(tipo_token, palavra_lida)
             else:
                 self.next = Token('IDEN', palavra_lida)
+        elif self.source[self.position] == '"' or self.source[self.position] == "'":
+            tipo_aspas = self.source[self.position] #p/ eu ver se são aspas simples ou duplas
+            self.position += 1
+            string_lida = ''
+            while self.position < len(self.source) and self.source[self.position] != tipo_aspas:
+                string_lida += self.source[self.position]
+                self.position += 1
+            if self.position >= len(self.source):
+                raise ValueError(f"[Lexer] String não fechada: '{string_lida}'")
+            self.position += 1
+
+            self.next = Token('STR', string_lida)
+
         else:
             raise ValueError(f"[Lexer] Caractere inválido: '{self.source[self.position]}'")
 
@@ -221,8 +305,9 @@ class Parser():
     def parse_expression():
         resultado = Parser.parse_term()
 
-        while Parser.lexer.next.type == "PLUS" or Parser.lexer.next.type == "MINUS" or Parser.lexer.next.type == "XOR" :
+        while Parser.lexer.next.type == "PLUS" or Parser.lexer.next.type == "MINUS" or Parser.lexer.next.type == "XOR"  or Parser.lexer.next.type == "CONCAT":
             operador = '+' if Parser.lexer.next.type == "PLUS" else ('-' if Parser.lexer.next.type == "MINUS" else "^")
+            operador = '..' if Parser.lexer.next.type == "CONCAT" else operador
             Parser.lexer.select_next()
             proximo_term = Parser.parse_term()
             resultado = BinOp(operador, [resultado, proximo_term])
@@ -243,6 +328,16 @@ class Parser():
             resultado = Parser.lexer.next.value
             Parser.lexer.select_next()
             resultado = IntVal(resultado, [])
+            return resultado
+        elif Parser.lexer.next.type == 'STR':
+            resultado = Parser.lexer.next.value
+            Parser.lexer.select_next()
+            resultado = StrVal(resultado, [])
+            return resultado
+        elif Parser.lexer.next.type == 'BOOL':
+            resultado = Parser.lexer.next.value
+            Parser.lexer.select_next()
+            resultado = BoolVal(resultado, [])
             return resultado
         elif Parser.lexer.next.type == 'IDEN':
             resultado = Parser.lexer.next.value
@@ -396,6 +491,21 @@ class Parser():
             if Parser.lexer.next.type != 'CLOSE_BRA': raise ValueError("[Parser] Esperado 'end'")
             Parser.lexer.select_next()
             return bloco
+        elif Parser.lexer.next.type == 'VAR':
+            Parser.lexer.select_next() #consumi o 'local'
+            if Parser.lexer.next.type != 'IDEN': 
+                raise ValueError("[Parser] Esperado identificador após 'local'")
+            nome_variavel = Parser.lexer.next.value
+            Parser.lexer.select_next()
+            if Parser.lexer.next.type != 'TYPE':
+                raise ValueError("[Parser] Esperado tipo após nome da variável")
+            tipo_variavel = Parser.lexer.next.value
+            Parser.lexer.select_next()
+            if Parser.lexer.next.type == 'ASSIGN':
+                Parser.lexer.select_next()
+                expression = Parser.parse_bool_expression()
+                return VarDec(tipo_variavel, [Identifier(nome_variavel, []), expression])
+            return VarDec(tipo_variavel, [Identifier(nome_variavel, [])])
         else:
             return NoOp()
         
